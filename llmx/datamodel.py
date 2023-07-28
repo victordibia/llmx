a786af4976612064cdf917f921ae6ab4baff2bbb
Victor Dibia <victor.dibia@gmail.com>
2023-07-28 12:13:00 -0700

    update datamodel

--- llmx/datamodel.py
+++ llmx/datamodel.py
@@ -1,12 +1,26 @@
-from typing import Any, Optional
+from dataclasses import asdict
+from typing import Any, Optional, Union
 from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class Message:
+    role: str
+    content: str
+
+    def __post_init__(self):
+        self._fields_dict = asdict(self)
+
+    def __getitem__(self, key: Union[str, int]) -> Any:
+        return self._fields_dict.get(key)
+
+    def to_dict(self):
+        return self._fields_dict
+
 
 @dataclass
 class TextGenerationConfig:
-    """Configuration for a text generation"""
     n: int = 1
-    # prompt: Optional[str] = None
-    # suffix: str = None
     temperature: float = 0.5
     max_tokens: int = None
     top_p: float = 1.0
@@ -14,14 +28,28 @@ class TextGenerationConfig:
     frequency_penalty: float = 0.0
     presence_penalty: float = 0.0
     model_type: str = "openai"
-    model: str = "gpt-3.5-turbo-0301"
-    messages: list[dict] = None
+    model: str = None
+    stop: Union[list[str], str, None] = None
+    messages: Union[list[dict], str, None] = None
+
+    def __post_init__(self):
+        self._fields_dict = asdict(self)
+
+    def __getitem__(self, key: Union[str, int]) -> Any:
+        return self._fields_dict.get(key)
 
 
 @dataclass
 class TextGenerationResponse:
     """Response from a text generation"""
-    text: list[Any]
+
+    text: list[Message]
     config: TextGenerationConfig
     logprobs: Optional[Any] = None
     usage: Optional[Any] = None
+
+    def __post_init__(self):
+        self._fields_dict = asdict(self)
+
+    def __getitem__(self, key: Union[str, int]) -> Any:
+        return self._fields_dict.get(key)
