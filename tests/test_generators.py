9623ba8ef03fa78c99c23573e95b29028f371314
Victor Dibia <victor.dibia@gmail.com>
2023-07-28 13:58:10 -0700

    update tests

---
+++ tests/test_generators.py
@@ -0,0 +1,41 @@
+from llmx.generators.text.textgen import TextGenerator
+from llmx.datamodel import TextGenerationConfig
+
+
+config = TextGenerationConfig(
+    n=2,
+    temperature=0.01,
+    max_tokens=100,
+    top_p=1.0,
+    top_k=50,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    messages=[
+        {"role": "user", "content": "What is the height of the Eiffel Tower?"},
+    ],
+)
+
+
+def test_openai():
+    openai_gen = TextGenerator(provider="openai")
+    openai_response = openai_gen.generate(config=config, use_cache=False)
+    answer = openai_response.text[0].content
+    print(openai_response.text[0].content)
+
+    assert (
+        "324" in answer or "1,063" in answer or "1,063 ft" in answer or "1063" in answer
+    )
+    assert len(openai_response.text) == 2
+
+
+def test_google():
+    google_gen = TextGenerator(provider="google")
+    config.model = "models/chat-bison-001"
+    google_response = google_gen.generate(config=config, use_cache=False)
+    answer = google_response.text[0].content
+    print(google_response.text[0].content)
+
+    assert (
+        "324" in answer or "1,063" in answer or "1,063 ft" in answer or "1063" in answer
+    )
+    assert len(google_response.text) == 2
