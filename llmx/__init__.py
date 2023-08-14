bad7df281e9819dbe9f2a56c562766f907b72890
Victor Dibia <victordibia@microsoft.com>
2023-08-13 20:48:49 -0700

    cli update

--- llmx/__init__.py
+++ llmx/__init__.py
@@ -2,7 +2,7 @@ import sys
 from .generators.text.textgen import llm
 from .datamodel import TextGenerationConfig, TextGenerationResponse, Message
 from .generators.text.base_textgen import TextGenerator
-
+from .generators.text.providers import providers
 
 if sys.version_info < (3, 10):
     raise RuntimeError("llmx requires Python 3.10+")
